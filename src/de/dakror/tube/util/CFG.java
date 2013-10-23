0876152561cd6c6dc89e22242438a422bb8c556b
evoltz <maxi990@gmx.de>
2013-10-23 14:29:20 +0200

    hi

--- src/de/dakror/tube/util/CFG.java
+++ src/de/dakror/tube/util/CFG.java
@@ -2,10 +2,11 @@ package de.dakror.tube.util;
 
 import java.io.File;
 
-public class CFG {
- public static final File DIR = new File (System.getProperty("user.home") +"/.dakror/Tube");
- static
- {
-  DIR.mkdir(); 
- }
+public class CFG
+{
+	public static final File DIR = new File(System.getProperty("user.home") + "/.dakror/Tube");
+	static
+	{
+		DIR.mkdirs();
+	}
 }
